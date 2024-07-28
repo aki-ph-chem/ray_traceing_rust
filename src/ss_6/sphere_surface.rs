46b885dbe461e662bde290221b7f849b684ca237
aki <aki@mymail.com>
2024-07-28 14:30:59 +0900

    surface normals

---
+++ src/ss_6/sphere_surface.rs
@@ -0,0 +1,91 @@
+use ray_tracing_rust::color::{write_color, Color};
+use ray_tracing_rust::ray::Ray;
+use ray_tracing_rust::vec3::{Point3, Vec3};
+use std::error::Error;
+use std::fs::File;
+use std::io::Write;
+
+fn hit_sphere(center: &Point3, radius: f64, ray: &Ray) -> f64 {
+    let oc = center.clone() - ray.origin().clone();
+    let a = Vec3::dot(ray.direction(), ray.direction());
+    let h = Vec3::dot(ray.direction(), &oc);
+    let c = Vec3::dot(&oc, &oc) - radius * radius;
+    let discriminant = h * h - a * c;
+
+    if discriminant < 0.0 {
+        -1.0
+    } else {
+        (h - discriminant.sqrt()) / a
+    }
+}
+
+fn ray_color(ray: &Ray) -> Color {
+    let t = hit_sphere(&Point3::from_slice([0.0, 0.0, -1.0]), 0.5, ray);
+    if t > 0.0 {
+        let normal = Vec3::new_unit_vec(ray.at(t) - Vec3::from_slice([0.0, 0.0, -1.0]));
+        return 0.5 * Color::from_slice([normal.x() + 1.0, normal.y() + 1.0, normal.z() + 1.0]);
+    }
+
+    let unit_direction = Vec3::new_unit_vec(ray.direction().clone());
+    let a = 0.5 * (unit_direction.y() + 1.0);
+
+    (1.0 - a) * Color::from_slice([1.0, 1.0, 1.0]) + a * Color::from_slice([0.5, 0.7, 1.0])
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let argv = std::env::args().collect::<Vec<String>>();
+    if argv.len() < 2 {
+        panic!("Error: invalid args");
+    }
+    let file_name = argv[1].clone();
+
+    // image
+    let aspect_ration = 16.0 / 9.0;
+    let image_width = 400;
+
+    // calculate the image height (Its ensure that it's at leat 1)
+    let image_height = (image_width as f64 / aspect_ration) as i32;
+    let image_height = if image_height < 1 { 1 } else { image_height };
+
+    // camera
+    let focal_lenth = 1.0;
+    let viewport_height = 2.0;
+    let viweport_width = viewport_height * (image_width as f64 / image_height as f64);
+    let camera_center = Point3::from_slice([0.0, 0.0, 0.0]);
+
+    // calculate the vector across the horizontal and down the vertical viewport edge.
+    let viewport_u = Vec3::from_slice([viweport_width, 0.0, 0.0]);
+    let viewport_v = Vec3::from_slice([0.0, -viewport_height, 0.0]);
+
+    // calculate the horizontal. and vertical delta vectors from pixel to pixel.
+    let pixel_delta_u = viewport_u.clone() / image_width as f64;
+    let pixel_delta_v = viewport_v.clone() / image_height as f64;
+
+    // calculate the location of the upper left pixel
+    let viewport_upper_left = camera_center.clone()
+        - Vec3::from_slice([0.0, 0.0, focal_lenth])
+        - viewport_u / 2.0
+        - viewport_v / 2.0;
+    let pixel100_loc = viewport_upper_left + 0.5 * (pixel_delta_u.clone() + pixel_delta_v.clone());
+
+    // render
+    let mut file = File::create(file_name)?;
+    let header = format!("P3\n{image_width} {image_height}\n255\n");
+    std::writeln!(&mut file, "{header}")?;
+    for j in 0..image_height {
+        eprintln!("\rScanlines remaining: {} ", image_height - j);
+        for i in 0..image_width {
+            let pixel_center = pixel100_loc.clone()
+                + (i as f64 * pixel_delta_u.clone())
+                + (j as f64 * pixel_delta_v.clone());
+            let ray_direction = pixel_center - camera_center.clone();
+            let ray = Ray::from_origin_dir(&camera_center, &ray_direction);
+
+            let pixel_color = ray_color(&ray);
+            write_color(&mut file, &pixel_color)?;
+        }
+    }
+    eprintln!("\rDone.   ");
+
+    Ok(())
+}
