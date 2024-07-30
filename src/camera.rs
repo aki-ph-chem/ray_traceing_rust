a6199c8c242faf574bd76229a85447e386c20741
aki <aki@mymail.com>
2024-07-31 01:34:21 +0900

    diffuse sphere: limit the recursion(child rays)

--- src/camera.rs
+++ src/camera.rs
@@ -13,6 +13,7 @@ pub struct Camera {
     pub aspect_ratio: f64,
     pub image_width: i32,
     pub samples_per_pixel: i32,
+    pub max_depth: i32,
     pixel_samples_scale: f64,
     image_height: i32,
     center: Point3,
@@ -28,6 +29,7 @@ impl Camera {
             aspect_ratio: 1.0,
             image_width: 100,
             samples_per_pixel: 10,
+            max_depth: 10,
             pixel_samples_scale: 0.0,
             image_height: 0,
             center: Point3::new(),
@@ -122,6 +124,34 @@ impl Camera {
         Ok(())
     }
 
+    pub fn render_diffuse_max_depth<T: HittableV2>(
+        &mut self,
+        world: &T,
+        file_name: &str,
+    ) -> Result<(), Box<dyn Error>> {
+        self.initialize();
+
+        let mut file = File::create(file_name)?;
+        let header = format!("P3\n{} {}\n255\n", self.image_width, self.image_height);
+        std::writeln!(&mut file, "{header}")?;
+        for j in 0..self.image_height {
+            eprintln!("\rScanlines remaining: {} ", self.image_height - j);
+            for i in 0..self.image_width {
+                let mut pixel_color = Color::new();
+                for _sample in 0..self.samples_per_pixel {
+                    let ray = self.get_ray(i, j);
+                    pixel_color += Self::ray_color_diffuse_max_depth(&ray, self.max_depth, world);
+                }
+                pixel_color *= self.pixel_samples_scale;
+                write_color(&mut file, &pixel_color)?;
+                pixel_color /= self.pixel_samples_scale;
+            }
+        }
+        eprintln!("\rDone.   ");
+
+        Ok(())
+    }
+
     fn initialize(&mut self) {
         // calculate the image height (Its ensure that it's at leat 1)
         self.image_height = (self.image_width as f64 / self.aspect_ratio) as i32;
@@ -199,6 +229,32 @@ impl Camera {
         ) {
             let direction = Vec3::random_on_hemisphere(&rec.normal);
             return 0.5 * Self::ray_color_diffuse(&Ray::from_origin_dir(&rec.p, &direction), world);
+        }
+
+        let unit_direction = Vec3::new_unit_vec(ray.direction().clone());
+        let a = 0.5 * (unit_direction.y() + 1.0);
+
+        (1.0 - a) * Color::from_slice([1.0, 1.0, 1.0]) + a * Color::from_slice([0.5, 0.7, 1.0])
+    }
+
+    pub fn ray_color_diffuse_max_depth<T: HittableV2>(ray: &Ray, depth: i32, world: &T) -> Color {
+        if depth <= 0 {
+            Color::from_slice([0.0, 0.0, 0.0]);
+        }
+
+        let mut rec = HitRecord::new();
+        if world.hit_v2(
+            &ray,
+            Interval::new_by_value(0.0, utl::constans::INFINITY),
+            &mut rec,
+        ) {
+            let direction = Vec3::random_on_hemisphere(&rec.normal);
+            return 0.5
+                * Self::ray_color_diffuse_max_depth(
+                    &Ray::from_origin_dir(&rec.p, &direction),
+                    depth - 1,
+                    world,
+                );
         }
 
         let unit_direction = Vec3::new_unit_vec(ray.direction().clone());
